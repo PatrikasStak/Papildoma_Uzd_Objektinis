db2fdcecc6f54ebf2c3203499b522e6f286bed21
Patrikas <patrestak@gmail.com>
2026-05-31 19:07:25 +0300

    v0.5

--- func.h
+++ func.h
@@ -1,3 +1,4 @@
 #include "lib.h"
 
 void Skaityti(const std::string& filename, std::map<std::string, std::vector<int>>& wordMap);
+void Rasyti(const std::string& countFilename, const std::string& linesFilename, const std::map<std::string, std::vector<int>>& wordMap);
--- main.cpp
+++ main.cpp
@@ -2,5 +2,7 @@
 
 int main() {
     std::map<std::string, std::vector<int>> wordMap;
+    Skaityti("text.txt", wordMap);
+    Rasyti("count.txt", "lines.txt", wordMap);
     return 0;
 }
